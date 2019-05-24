9c9bf83a3eef6c3ec645b1ebd4fc0802de3563c4
vstuber <stuber@leadingsystems.de>
2019-05-24 11:49:56 +0200

    Support nested element folders

--- core/modules/elementFolder/elementFolderInstance/view.js
+++ core/modules/elementFolder/elementFolderInstance/view.js
@@ -10,7 +10,9 @@ var obj_classdef = 	{
 	start: function() {
 	    var str_options,
             obj_options,
-            obj_unfoldOptions;
+            obj_unfoldOptions,
+            el_elementFolderToggler,
+            el_elementFolderContent;
 
 		this.registerElements(this.__el_container, 'main', true);
 
@@ -24,13 +26,19 @@ var obj_classdef = 	{
 
         this.str_containerID = this.__el_container.getProperty('id');
 
+        /*
+         * In order to support nested element folders we use only the first toggler and content element that has been found
+         */
+        el_elementFolderToggler = this.__autoElements.main.elementFolderToggler[0];
+        el_elementFolderContent = this.__autoElements.main.elementFolderContent[0];
+
         obj_unfoldOptions = Object.merge(
             {
                 str_initialToggleStatus: 'open',
                 bln_toggleOnInitialization: false,
                 bln_skipAnimationWhenTogglingOnInitialization: false,
-                var_togglerSelector: this.__autoElements.main.elementFolderToggler,
-                var_contentBoxSelector: this.__autoElements.main.elementFolderContent,
+                var_togglerSelector: el_elementFolderToggler,
+                var_contentBoxSelector: el_elementFolderContent,
                 var_wrapperSelector: this.__el_container,
                 str_cookieIdentifierName: this.str_containerID ? this.str_containerID : '',
                 str_initialCookieStatus: 'open',
