5fedf1fb22a4ee127d415c3259da17a60f9fcf0c
vstuber <stuber@leadingsystems.de>
2019-07-01 09:01:53 +0200

    Make it possible to use foldedNavi on multiple elements at once using only one selector

--- core/modules/foldedNavi/view.js
+++ core/modules/foldedNavi/view.js
@@ -6,38 +6,48 @@ var str_moduleName = '__moduleName__';
 
 var obj_classdef = 	{
 	start: function() {
-		this.__el_container =
+		this.__els_container =
 			(
 				typeOf(this.__models.options.data.var_naviSelector) === 'element'
 				||	typeOf(this.__models.options.data.var_naviSelector) === 'null'
 			)
 				?	this.__models.options.data.var_naviSelector
-				:	$$(this.__models.options.data.var_naviSelector)[0];
+				:	$$(this.__models.options.data.var_naviSelector);
 
-		if (typeOf(this.__el_container) !== 'element') {
+		if (typeOf(this.__els_container) !== 'element' && typeOf(this.__els_container) !== 'elements') {
 			return;
 		}
 
-		this.__el_container.addClass('useFoldedNavi');
+		if (typeOf(this.__els_container) !== 'elements') {
+            this.__els_container = new Elements([this.__els_container]);
+		}
 
-		Array.each(this.__el_container.getElements('li').filter(this.__models.options.data.bln_onlyFirstLevel ? '.level_1 > .submenu' : '.submenu'), function(el_li) {
-			var el_toggler = new Element('span.foldedNaviToggler').inject(el_li, 'top');
+		this.__els_container.addClass('useFoldedNavi');
 
-			lsjs.__moduleHelpers.unfold.start({
-				bln_automaticallyCreateResizeBox: false,
-				str_initialDisplayType: 'block',
-				str_initialToggleStatus: 'closed',
-				bln_toggleOnInitialization: this.__models.options.data.bln_automaticallyToggleOnInitialization ? (el_li.hasClass('trail') || el_li.hasClass('active')) : false,
-				var_togglerSelector: el_toggler,
-				var_contentBoxSelector: el_li.getElement('> ul'),
-				var_wrapperSelector: el_li,
-				str_animationMode: 'height',
-				obj_morphOptions: {
-					'duration': 400
-				},
-				bln_closeOnOutsideClick: this.__models.options.data.bln_closeOnOutsideClick
-			});
-		}.bind(this));
+		Array.each(
+            this.__els_container,
+			function(el_naviToFold) {
+				Array.each(el_naviToFold.getElements('li').filter(this.__models.options.data.bln_onlyFirstLevel ? '.level_1 > .submenu' : '.submenu'), function(el_li) {
+					var el_toggler = new Element('span.foldedNaviToggler').inject(el_li, 'top');
+
+					lsjs.__moduleHelpers.unfold.start({
+						bln_automaticallyCreateResizeBox: false,
+						str_initialDisplayType: 'block',
+						str_initialToggleStatus: 'closed',
+						bln_toggleOnInitialization: this.__models.options.data.bln_automaticallyToggleOnInitialization ? (el_li.hasClass('trail') || el_li.hasClass('active')) : false,
+						var_togglerSelector: el_toggler,
+						var_contentBoxSelector: el_li.getElement('> ul'),
+						var_wrapperSelector: el_li,
+						str_animationMode: 'height',
+						obj_morphOptions: {
+							'duration': 400
+						},
+						bln_closeOnOutsideClick: this.__models.options.data.bln_closeOnOutsideClick
+					});
+				}.bind(this));
+			}.bind(this)
+		);
+
 	}
 };
 
