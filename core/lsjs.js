749d75b178adc73c69b210bbd648ce209ee64416
Volker Stuber <stuber@leadingsystems.de>
2021-03-19 14:31:33 +0100

    Fix in Request.cajax - for reasons yet unknown options.url could be both a string or an object in different situations and checking for the ? character must work in both cases

--- core/lsjs.js
+++ core/lsjs.js
@@ -55,7 +55,7 @@ Request.cajax = new Class({
 	initialize: function(options){
 
 		if (options.url !== undefined && options.url !== null) {
-			options.url = options.url + (options.url.includes('?') ? '&' : '?') + 'cajaxCall=' + (new Date).getTime();
+			options.url = options.url + (String(options.url).includes('?') ? '&' : '?') + 'cajaxCall=' + (new Date).getTime();
         }
 
 		this.parent(options);
