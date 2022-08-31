30a9338ef6b5cb32a2589b5db58faeef93495d13
tbu123 <buchmann@leadingsystems.de>
2022-08-31 11:15:51 +0200

    numberStepper, bearbeitet, WIP

--- core/modules/numberStepper/models/lang.js
+++ core/modules/numberStepper/models/lang.js
@@ -1,11 +1,6 @@
 var obj_classdef_model = {
 	name: 'lang',
 	
-	//data: {
-		//selectAllLabel: 'Alle ausw&auml;hlen',
-		//myLabel: 'Meine Sprachvariable fürs Label'
-	//},
-
 	start: function() {
 		this.__module.onModelLoaded();
 	}
