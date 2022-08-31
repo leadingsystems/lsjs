87f38f6fb6d7a6de0dbb8d6a8780a617541d80d1
tbu123 <buchmann@leadingsystems.de>
2022-08-31 09:06:03 +0200

    numberStepper, Testcode/Kommentare/überflüssiges entfernt, WIP

--- core/modules/numberStepper/models/lang.js
+++ core/modules/numberStepper/models/lang.js
@@ -1,10 +1,10 @@
 var obj_classdef_model = {
 	name: 'lang',
 	
-	data: {
-		selectAllLabel: 'Alle ausw&auml;hlen',
-		myLabel: 'Meine Sprachvariable fürs Label'
-	},
+	//data: {
+		//selectAllLabel: 'Alle ausw&auml;hlen',
+		//myLabel: 'Meine Sprachvariable fürs Label'
+	//},
 
 	start: function() {
 		this.__module.onModelLoaded();
